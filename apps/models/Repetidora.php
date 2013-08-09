5cc1157adbd2cab4bd7e21d6da8125a29cb9664e
Alexsandro Pereira <apssouza22@gmail.com>
2013-08-09 14:48:59 -0300

    transferindo da salve

---
+++ apps/models/Repetidora.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+ * Cuida dos cadastros de tecnicos
+ *
+ * @author apssouza
+ */
+class Repetidora extends Model
+{
+	const TB_NAME = 'repetidora';
+	const PG_LISTAR = 'listar.php';
+	const PG_DETALHE = 'detalhes.php';
+	const PG_EDITAR = 'form.php';
+	const PG_PASTA = 'repetidora';
+	
+	
+	
+}
+
+?>
